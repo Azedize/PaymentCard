45bbb59a4c8e2cbe89c6d14173e1f010464c75a8
Azedine Chentouf <148768155+Azedize@users.noreply.github.com>
2025-05-17 23:42:14 +0100

    Update server.js

--- server.js
+++ server.js
@@ -40,8 +40,8 @@ app.post('/api/create-checkout-session', async (req, res) => {
         quantity: 1,
       }],
       mode: 'payment',
-      success_url: 'http://localhost:3000/success.html?session_id={CHECKOUT_SESSION_ID}',
-      cancel_url: 'http://localhost:3000/cancel.html'
+      success_url: 'https://payment-card-jade.vercel.app/success.html?session_id={CHECKOUT_SESSION_ID}',
+      cancel_url: 'https://payment-card-jade.vercel.app/cancel.html'
     });
 
     res.json({ url: session.url });
