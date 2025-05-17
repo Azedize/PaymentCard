59b00f006fa80fbd8dffb1b9ad7042a4cc3721f5
Azedine Chentouf <148768155+Azedize@users.noreply.github.com>
2025-05-17 23:41:36 +0100

    Update server.js

--- server.js
+++ server.js
@@ -40,8 +40,8 @@ app.post('/api/create-checkout-session', async (req, res) => {
         quantity: 1,
       }],
       mode: 'payment',
-      success_url: '/success.html?session_id={CHECKOUT_SESSION_ID}',
-      cancel_url: '/cancel.html'
+      success_url: 'http://localhost:3000/success.html?session_id={CHECKOUT_SESSION_ID}',
+      cancel_url: 'http://localhost:3000/cancel.html'
     });
 
     res.json({ url: session.url });
