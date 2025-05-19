a8b328784db0ec5b5d68c16276c0237d9e32e2ca
Azedine Chentouf <148768155+Azedize@users.noreply.github.com>
2025-05-19 21:05:47 +0100

    Update script.js

--- public/script.js
+++ public/script.js
@@ -18,3 +18,29 @@ document.addEventListener('DOMContentLoaded', function() {
 
   });
 
+ paypal.Buttons({
+createOrder: function(data, actions) {
+  if (!validateForm()) {
+    alert('Veuillez remplir tous les champs requis.');
+    return actions.reject(); // Annule le paiement
+  }
+
+  const amountInCents = '10.00'; // Montant à payer
+  return actions.order.create({
+    purchase_units: [{
+      amount: {
+        value: amountInCents
+      }
+    }]
+  });
+},
+onApprove: function(data, actions) {
+  return actions.order.capture().then(function(details) {
+    alert('Transaction réussie !\n' + details.payer.name.given + ': ' + details.id);
+  });
+},
+onError: function(err) {
+  console.error('Erreur lors du paiement', err);
+  alert('Une erreur est survenue lors du traitement du paiement.');
+}
+}).render('#paypal-button-container'); // Affiche le bouton de paiement
